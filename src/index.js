1a852e06e135fe91e68537a8aa34029e06a188e0
MotherOfParrots <105819694+MotherOfParrots@users.noreply.github.com>
2022-08-13 00:08:51 +0700

    feat: function reverse some numbers

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function reverse (n) {
-  
+  const arr = n.toString().split('');
+  console.log(arr);
+  const noMinus = arr.slice(1);
+  if (arr[0] === '-') {
+    return noMinus;
+  } else {
+    return arr.reverse().join('');
+  }
 }
+
